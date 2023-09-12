1e82dc6c39f17f254264b4532475c339da80b66c
MrSecant <97333782+MrSecant@users.noreply.github.com>
2023-09-12 10:12:04 +0800

    Update eval.sh

--- manipulation/eval.sh
+++ manipulation/eval.sh
@@ -1,7 +1,7 @@
 python ./test_manipulation.py \
---data_split ./mobility_dataset \      # your path to mobility_dataset
---test_data ./test_data \              # your path to test data
+--data_split ./mobility_dataset \# your path to mobility_dataset
+--test_data ./test_data \# your path to test data
 --mode manipulation \
---model_path ./ckpts/model_best.pth \  # your path to pretrained model
---cfg_file ./config/config.yaml \      # your path to config
---save_path ./exp                      # the path to save results of manipulation 
+--model_path ./ckpts/model_best.pth \# your path to pretrained model
+--cfg_file ./config/config.yaml \# your path to config
+--save_path ./exp# the path to save results of manipulation 
